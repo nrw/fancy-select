44f298575a0643b17f921cf8a2680f8c5cb096ba
Nicholas Westlake <nicholasredlin@gmail.com>
2014-08-21 01:13:44 -0500

    compatibility with phantomjs

--- test/index.js
+++ test/index.js
@@ -1,3 +1,17 @@
+var test = require('tape')
+
+var mercury = require('mercury')
+
+// FFFfffff--- phantomJS.
+if (!Function.prototype.bind) {
+  Function.prototype.bind = require('function-bind')
+}
+
+test('mercury is a object', function (t) {
+  t.equal(typeof mercury, 'object')
+  t.end()
+})
+
 require('./navigate.js')
 require('./nested-filter.js')
 require('./integration.js')
