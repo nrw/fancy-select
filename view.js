07ceb6686524270f0d11e6d3c23d796f742d58ad
Nicholas Westlake <nicholasredlin@gmail.com>
2014-09-10 03:33:38 -0500

    move string measurement to another module

--- view.js
+++ view.js
@@ -5,6 +5,9 @@ var mutableFocus = require('./mutable-focus')
 var fs = require('fs')
 var insertCss = require('insert-css')
 
+var arrayEqual = require('array-equal')
+var stringWidth = require('styled-string-width')
+
 insertCss(fs.readFileSync(__dirname + '/styles/core.css', 'utf8'))
 
 var ENTER = 13
@@ -82,87 +85,59 @@ function renderGroup (state, items, base) {
   })
 }
 
+var inputEvents = {}
+inputEvents.blur = function (state, e) {
+  var relatedTarget = e.relatedTarget
+
+  // TODO: this will likely stop working until
+  // https://github.com/Raynos/dom-delegator/pull/4 is fixed
+  if (
+    !relatedTarget ||
+    !e.currentTarget.parentNode.parentNode.contains(relatedTarget)
+  ) {
+    state.events.dropdown(false)
+  }
+}
+inputEvents.focus = function (state, e) {
+  state.events.dropdown(true)
+}
+
+inputEvents.keydown = function (state, e) {
+  switch (e.keyCode) {
+    case BACKSPACE:
+      state.events.backspace()
+      break
+    case COMMA:
+    case ENTER:
+      state.events.select(state.active)
+      e.preventDefault()
+      break
+    case DOWN:
+      state.events.next()
+      e.preventDefault()
+      break
+    case UP:
+      state.events.prev()
+      e.preventDefault()
+      break
+    case ESCAPE:
+      e.currentTarget.blur()
+      break
+  }
+}
+
 function inputEvent (state, e) {
   mutableFocus()
-
-  if (e.type === 'blur') {
-    var relatedTarget = e.relatedTarget
-
-    // TODO: this will likely stop working until
-    // https://github.com/Raynos/dom-delegator/pull/4 is fixed
-    if (
-      !relatedTarget ||
-      !e.currentTarget.parentNode.parentNode.contains(relatedTarget)
-    ) {
-      state.events.dropdown(false)
-    }
-  } else if (e.type === 'focus') {
-    state.events.dropdown(true)
-  } else if (e.type === 'keydown') {
-    switch (e.keyCode) {
-      case BACKSPACE:
-        state.events.backspace()
-        break
-      case COMMA:
-      case ENTER:
-        state.events.select(state.active)
-        e.preventDefault()
-        break
-      case DOWN:
-        state.events.next()
-        e.preventDefault()
-        break
-      case UP:
-        state.events.prev()
-        e.preventDefault()
-        break
-      case ESCAPE:
-        e.currentTarget.blur()
-        break
-    }
+  if (inputEvents[e.type]) {
+    inputEvents[e.type](state, e)
   }
 }
 
 function maxWidth (strs) {
   var one, max = 0
   strs.forEach(function (str) {
-    one = measureString(str)
-    if (one > max) {
-      max = one
-    }
+    one = stringWidth(str, '.fancy-select input')
+    if (one > max) max = one
   })
   return max
-}
-
-function measureString (str, current) {
-  if (!str) return 0
-
-  var span = document.createElement('span')
-  span.style = 'position: absolute; top: -99999; left: -99999; width: auto;' +
-    'padding: 0; white-space: pre;'
-  span.innerText = str
-  document.body.appendChild(span)
-
-  // TODO: copy styles from input computed style to tolerate font changes
-  // document.querySelector('.fancy-select input')
-  // var currentStyle = window.getComputedStyle(current, null)
-  // var toCopy = ['letterSpacing', 'fontSize', 'fontFamily', 'fontWeight',
-  //   'textTransform']
-  // toCopy.forEach(function (one) {
-  //   test.style[one] = currentStyle.style[one]
-  // })
-
-  var width = span.offsetWidth
-  span.remove()
-  return width
-}
-
-function arrayEqual (a, b) {
-  if (!Array.isArray(a) || !Array.isArray(b)) return false
-  if (a.length !== b.length) return false
-
-  for (var i = a.length - 1; i >= 0; i--) {
-    if (a[i] !== b[i]) return false
-  }
-  return true
 }
